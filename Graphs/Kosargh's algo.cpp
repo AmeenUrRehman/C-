c62a07971d26411edd926154e037277c68e71076
Ameen Ur Rehman <ameenreh2003@gmail.com>
2023-02-20 13:06:05 +0530

    kosragh's algo

---
+++ Graphs/Kosargh's algo.cpp
@@ -0,0 +1,69 @@
+#include<unordered_map>
+#include<stack>
+#include<list>
+
+void dfs(int node,unordered_map<int , bool> &vis, stack<int> &st, unordered_map<int , list<int>> &adj){
+	vis[node] = true;
+	for(auto nbr : adj[node]){
+		if(!vis[nbr]){
+			dfs(nbr,vis,st,adj);
+		}
+	}
+	// Topo Logic
+	st.push(node);
+}
+
+void revdfs(int node, unordered_map<int , bool> &vis,unordered_map<int , list<int>> &adj ){
+	
+	vis[node] = true;
+	for(auto nbr : adj[node]){
+		if(!vis[nbr]){
+			revdfs(nbr,vis,adj);
+		}
+	}
+	
+}
+
+int stronglyConnectedComponents(int v, vector<vector<int>> &edges)
+{
+	// Create Adj List
+	unordered_map<int , list<int>> adj;
+	for(int i = 0 ; i < edges.size() ; i++){
+		int u = edges[i][0];
+		int v = edges[i][1];
+		adj[u].push_back(v);
+	}
+
+	// TopoLogical Sort
+	stack<int> st;
+	unordered_map<int,bool> vis;
+	for(int i = 0 ; i < v ; i++){
+		if(!vis[i]){
+			dfs(i,vis,st,adj);
+		}
+	}
+
+	//Create a transpose graph
+	unordered_map<int , list<int>> transpose;
+	for(int i = 0 ; i < v ; i++){
+		vis[i] = 0;
+		for(auto nbr : adj[i]){
+			transpose[nbr].push_back(i);
+		}
+	}
+
+
+	// DFS call 
+	int count = 0;
+	while(!st.empty()){
+		int top = st.top();
+		st.pop();
+		if(!vis[top]){
+			count++;
+			revdfs(top,vis,transpose);
+		}
+	}
+	
+	return count;
+
+}
