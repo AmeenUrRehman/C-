bd8bbe7bd3c3e83f08b1e119c816e49deca2a95a
Ameen Ur Rehman <ameenreh2003@gmail.com>
2023-03-11 22:02:40 +0530

    12

---
+++ Dynamic Programming/PerfectSquare/MinmumSquare.cpp
@@ -0,0 +1,27 @@
+class Solution{
+	public:
+	
+	int solveMem(int n , vector<int> &dp){
+	    //Base Case
+	    if(n == 0){
+	        return 0;
+	    }
+	    
+	    if(dp[n] != -1){
+	        return dp[n];
+	    }
+	    
+	    int ans = n;
+	    for(int i = 1 ; i*i <= n ; i++){
+	        int temp = i*i;
+	        ans = min(ans , 1 + solveMem(n - temp , dp));
+	    }
+	    dp[n] = ans;
+	    return dp[n];
+	}
+	int MinSquares(int n)
+	{
+	   vector<int> dp(n+1, -1);
+	   return solveMem(n , dp);
+	}
+};
